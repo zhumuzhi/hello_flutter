15e5018f4cd772ae698089cde334cf88151575fa
Zhumz <zhumuzhi@icloud.com>
2020-08-17 15:14:40 +0800

    点击事件

--- lib/sampleExample/InteractionSample.dart
+++ lib/sampleExample/InteractionSample.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-
 class TapboxASample extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -10,14 +10,24 @@ class TapboxASample extends StatelessWidget {
         appBar: new AppBar(
           title: new Text('TapboxA Demo'),
         ),
-        body: new Center(
-          child: new TapboxA(),
+
+        body: new Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new TapboxA(),
+            new TapboxBParentWidget(),
+          ],
         ),
+
       ),
 
     );
   }
 }
+
+
+//------------------------ TapboxA --------------------------------
 
 class TapboxA extends StatefulWidget {
   TapboxA({Key key}) : super(key: key);
@@ -32,6 +42,7 @@ class _TapboxAState extends State<TapboxA> {
     setState(() {
       _active = !_active;
     });
+    print('点击了TapboxA--状态为:$_active');
   }
 
   @override
@@ -54,36 +65,61 @@ class _TapboxAState extends State<TapboxA> {
   }
 }
 
-class ParentWidget extends StatefulWidget {
+
+//------------------------ TapboxB --------------------------------
+
+class TapboxBParentWidget extends StatefulWidget {
   @override
   _ParentWidgetState createState() => new _ParentWidgetState();
 }
 
-class _ParentWidgetState extends State<ParentWidget> {
+class _ParentWidgetState extends State<TapboxBParentWidget> {
   bool _active = false;
 
   void _handleTapboxChange(bool newValue) {
     setState(() {
       _active = newValue;
     });
+    print('点击了TapboxB--状态为:$_active');
   }
 
   @override
   Widget build(BuildContext context) {
     return new Container(
-//      child: new TapboxB(
-//
-//      ),
+      child: new TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChange,
+      ),
     );
   }
-
 }
 
 class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false,@required this.onChanged}) : super(key: key);
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0,color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
   }
 }

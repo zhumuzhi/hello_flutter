73a2bef0fb078b9fdb5f97f84c13bde67bd190c0
Zhumz <zhumuzhi@icloud.com>
2020-08-25 11:53:13 +0800

    控件调整

--- lib/Widget/WidgetExmple/widget_ListView.dart
+++ lib/Widget/WidgetExmple/widget_ListView.dart
@@ -10,13 +10,13 @@ class ListViewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 1,
+      length: 3,
       child: Scaffold(
         body: TabBarView(
           children: [
             ParallelWidget(),
-//            ScrollNotificationWidget(),
-//            ScrollControllerWidget()
+            ScrollNotificationWidget(),
+            ScrollControllerWidget()
           ],
         ),
         bottomNavigationBar: TabBar(
@@ -27,15 +27,17 @@ class ListViewWidget extends StatelessWidget {
             tabs: [
               Tab(
                 icon: Icon(Icons.home),
-                text: "视差",
+                text: "CustomScrollView",
               ),
-//              Tab(icon: Icon(Icons.rss_feed),text: "Notification",),
-//              Tab(icon: Icon(Icons.perm_identity),text: "Controller",)
+              Tab(icon: Icon(Icons.rss_feed),text: "Notification",),
+              Tab(icon: Icon(Icons.perm_identity),text: "Controller",)
             ]),
       ),
     );
   }
 }
+
+
 
 class ParallelWidget extends StatelessWidget {
   @override
@@ -62,32 +64,33 @@ class ParallelWidget extends StatelessWidget {
 
 
 
-//class ScrollNotificationWidget extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//        title: 'Scroll Notification Demo',
-//        home: Scaffold(
-//            appBar: AppBar(title: Text('ScrollController Demo')),
-//            body: NotificationListener<ScrollNotification>(// 添加 NotificationListener 作为父容器
-//              onNotification: (scrollNotification) {// 注册通知回调
-//                if (scrollNotification is ScrollStartNotification) {// 滚动开始
-//                  print('Scroll Start');
-//                } else if (scrollNotification is ScrollUpdateNotification) {// 滚动位置更新
-//                  print('Scroll Update');
-//                } else if (scrollNotification is ScrollEndNotification) {// 滚动结束
-//                  print('Scroll End');
-//                }
-//              },
-//              child: ListView.builder(
-//                itemCount: 30,// 列表元素个数
-//                itemBuilder: (context, index) => ListTile(title: Text("Index : $index")),// 列表项创建方法
-//              ),
-//            )
-//        )
-//    );
-//  }
-//}
+class ScrollNotificationWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Scroll Notification Demo',
+        home: Scaffold(
+            appBar: AppBar(title: Text('ScrollController Demo')),
+            body: NotificationListener<ScrollNotification>(// 添加 NotificationListener 作为父容器
+              onNotification: (scrollNotification) {// 注册通知回调
+                if (scrollNotification is ScrollStartNotification) {// 滚动开始
+                  print('Scroll Start');
+                } else if (scrollNotification is ScrollUpdateNotification) {// 滚动位置更新
+                  print('Scroll Update');
+                } else if (scrollNotification is ScrollEndNotification) {// 滚动结束
+                  print('Scroll End');
+                }
+                return true;
+              },
+              child: ListView.builder(
+                itemCount: 30,// 列表元素个数
+                itemBuilder: (context, index) => ListTile(title: Text("Index : $index")),// 列表项创建方法
+              ),
+            )
+        )
+    );
+  }
+}
 
 
 class ScrollControllerWidget extends StatefulWidget {

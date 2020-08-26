a16bc3822f5feff37bb04b0cf023831084b643bb
Zhumz <zhumuzhi@icloud.com>
2020-08-26 18:15:06 +0800

    删除无用代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/BottomNavigation/BottomNavigationBar.dart';
 
-void main() {
-
-  var p = new Point(100, 200); // new ???????
-  p.printInfo(); // ???100?200?;
-  Point.factor = 10;
-  Point.printZValue(); // ??10
-
-}
-
-class Point {
-  // ???-????0
-  static num factor = 0;
-  // ????
-  num x, y;
-  // ??????????????(?????????????this.x = x; this.y = y;)
-  Point(this.x, this.y);
-  // ????
-  void printInfo() => print('$x, $y');
-  // ???
-  static void printZValue() => print('factor'); //???
-}
-
-
-
-
-//void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -36,4 +11,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-

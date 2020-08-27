bff295216757ec3f4af09f3197b61fffb2adbee2
Zhumz <zhumuzhi@icloud.com>
2020-08-27 15:58:54 +0800

    格式优化

--- lib/Widget/WidgetExmple/widget_ListView.dart
+++ lib/Widget/WidgetExmple/widget_ListView.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class ListViewWidget extends StatelessWidget {
   final String title;
 
@@ -29,40 +28,48 @@ class ListViewWidget extends StatelessWidget {
                 icon: Icon(Icons.home),
                 text: "CustomScrollView",
               ),
-              Tab(icon: Icon(Icons.rss_feed),text: "Notification",),
-              Tab(icon: Icon(Icons.perm_identity),text: "Controller",)
+              Tab(
+                icon: Icon(Icons.rss_feed),
+                text: "Notification",
+              ),
+              Tab(
+                icon: Icon(Icons.perm_identity),
+                text: "Controller",
+              )
             ]),
       ),
     );
   }
 }
 
-
-
 class ParallelWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return
-      CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(//SliverAppBar 作为头图控件
-              automaticallyImplyLeading: true,
-              title: Text('CustomScrollView Demo'), // 标题
-              floating: true,// 设置悬浮样式
-              flexibleSpace: Image.network("https://media-cdn.tripadvisor.com/media/photo-s/13/98/8f/c2/great-wall-hiking-tours.jpg",fit:BoxFit.cover),// 设置悬浮头图背景
-              expandedHeight: 280,// 头图控件高度
-            ),
-            SliverList(//SliverList 作为列表控件
-              delegate: SliverChildBuilderDelegate(
-                    (context, index) => ListTile(title: Text('Item #$index')),// 列表项创建方法
-                childCount: 100,// 列表元素个数
-              ),
-            ),
-          ]);
+    return CustomScrollView(slivers: <Widget>[
+      SliverAppBar(
+        //SliverAppBar 作为头图控件
+        automaticallyImplyLeading: true,
+        title: Text('CustomScrollView Demo'),
+        // 标题
+        floating: true,
+        // 设置悬浮样式
+        flexibleSpace: Image.network(
+            "https://media-cdn.tripadvisor.com/media/photo-s/13/98/8f/c2/great-wall-hiking-tours.jpg",
+            fit: BoxFit.cover),
+        // 设置悬浮头图背景
+        expandedHeight: 280, // 头图控件高度
+      ),
+      SliverList(
+        //SliverList 作为列表控件
+        delegate: SliverChildBuilderDelegate(
+          (context, index) => ListTile(title: Text('Item #$index')),
+          // 列表项创建方法
+          childCount: 100, // 列表元素个数
+        ),
+      ),
+    ]);
   }
 }
-
-
 
 class ScrollNotificationWidget extends StatelessWidget {
   @override
@@ -71,27 +78,30 @@ class ScrollNotificationWidget extends StatelessWidget {
         title: 'Scroll Notification Demo',
         home: Scaffold(
             appBar: AppBar(title: Text('ScrollController Demo')),
-            body: NotificationListener<ScrollNotification>(// 添加 NotificationListener 作为父容器
-              onNotification: (scrollNotification) {// 注册通知回调
-                if (scrollNotification is ScrollStartNotification) {// 滚动开始
+            body: NotificationListener<ScrollNotification>(
+              // 添加 NotificationListener 作为父容器
+              onNotification: (scrollNotification) {
+                // 注册通知回调
+                if (scrollNotification is ScrollStartNotification) {
+                  // 滚动开始
                   print('Scroll Start');
-                } else if (scrollNotification is ScrollUpdateNotification) {// 滚动位置更新
+                } else if (scrollNotification is ScrollUpdateNotification) {
+                  // 滚动位置更新
                   print('Scroll Update');
-                } else if (scrollNotification is ScrollEndNotification) {// 滚动结束
+                } else if (scrollNotification is ScrollEndNotification) {
+                  // 滚动结束
                   print('Scroll End');
                 }
                 return true;
               },
               child: ListView.builder(
-                itemCount: 30,// 列表元素个数
-                itemBuilder: (context, index) => ListTile(title: Text("Index : $index")),// 列表项创建方法
+                itemCount: 30, // 列表元素个数
+                itemBuilder: (context, index) =>
+                    ListTile(title: Text("Index : $index")), // 列表项创建方法
               ),
-            )
-        )
-    );
+            )));
   }
 }
-
 
 class ScrollControllerWidget extends StatefulWidget {
   @override
@@ -158,7 +168,4 @@ class _ScrollControllerState extends State<ScrollControllerWidget> {
     _controller.dispose(); // 销毁控制器
     super.dispose();
   }
-
 }
-
-

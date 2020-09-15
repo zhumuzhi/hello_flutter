19f5fd571fd11087d9d1d75894c87d9f8825962f
Zhumz <zhumuzhi@icloud.com>
2020-09-15 09:47:16 +0800

    调整路由案例

--- lib/hf_examplePage/sampleExample/Example_RouteSample.dart
+++ lib/hf_examplePage/sampleExample/Example_RouteSample.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/Support/define/define.dart';
 
-
-
 class RouteSample extends StatefulWidget {
   @override
   _RouteSampleState createState() => _RouteSampleState();
 }
 
-class _RouteSampleState extends State<RouteSample> with SingleTickerProviderStateMixin{
-
+class _RouteSampleState extends State<RouteSample>
+    with SingleTickerProviderStateMixin {
   TabController _tabController;
-  List<String> tabList = ["基本路由","命名路由"];
+  List<String> tabList = ["基本路由", "命名路由"];
 
   @override
   void initState() {
@@ -35,49 +33,146 @@ class _RouteSampleState extends State<RouteSample> with SingleTickerProviderStat
       body: TabBarView(
         controller: _tabController,
         children: <Widget>[
-          FirstPage(),
-          FirstPage(),
+          BaseRoutePage(),
+          NamedRoute(),
         ],
       ),
     );
   }
 }
 
+/// ========== 基本路由 ==========
 
+class BaseRoutePage extends StatefulWidget {
+  @override
+  _BaseRoutePageState createState() => _BaseRoutePageState();
+}
 
-class FirstPage extends StatelessWidget {
+class _BaseRoutePageState extends State<BaseRoutePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-            child: Text('EventButton'),
-            color: randomColor(),
-            onPressed: () {
-              print("跳转至下级页面");
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SecondPage()));
-            }),
+    return Center(
+      child: Scaffold(
+        body: Center(
+          child: Column(children: <Widget>[
+            RaisedButton(
+                child: Text('基本路由'),
+                color: randomColor(),
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SecondPage()))),
+          ]),
+        ),
       ),
     );
   }
 }
 
+/// ========== 命名路由 ==========
+
+class NamedRoute extends StatefulWidget {
+  @override
+  _NamedRouteState createState() => _NamedRouteState();
+}
+
+class _NamedRouteState extends State<NamedRoute> {
+  String _msg = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(children: <Widget>[
+          RaisedButton(
+              child: Text('命名路由'),
+              color: randomColor(),
+              onPressed: () => Navigator.pushNamed(context, 'secondPage')),
+          RaisedButton(
+              child: Text('命名路由(参数&回调)'),
+              color: randomColor(),
+              onPressed: () =>
+                  Navigator.pushNamed(context, 'thirdPage', arguments: 'Hey')
+                      .then((value) {
+                    setState(() {
+                      _msg = value;
+                    });
+                  })),
+          Text('Message from Second screen: $_msg'),
+          RaisedButton(
+            child: Text('命名路由异常处理'),
+            onPressed: () => Navigator.pushNamed(context, 'UnknownPage'),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+/// ========== SecondPage ==========
 
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('SecondPage'),leading: null,),
+      appBar: AppBar(
+        title: Text('SecondPage'),
+        leading: null,
+      ),
       body: Center(
         child: RaisedButton(
-            child: Text('EventButton'),
+            child: Text('返回上级页面'),
             color: randomColor(),
-            onPressed: () { 
-              print("返回上级页面");
+            onPressed: () {
               Navigator.pop(context);
             }),
       ),
     );
   }
 }
+
+/// ========== ThirdPage ==========
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String msg = ModalRoute.of(context).settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Screen'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text('Message from first screen: $msg'),
+          RaisedButton(
+            child: Text('back'),
+            onPressed: () => Navigator.pop(context, 'Hi'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/// ========== UnknownPage ==========
+
+class UnknownPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('Unknown Screen'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Back'),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/hf_bottomNavigation/hf_bottomNavigationBar.dart';
 
+import 'package:hello_flutter/hf_examplePage/sampleExample/Example_RouteSample.dart';
+
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        "secondPage":(context) => SecondPage(),
+        "thirdPage":(context) => ThirdPage(),
+      },
+      onUnknownRoute: (RouteSettings setting) => MaterialPageRoute(builder: (context) => UnknownPage()),
       home: TabsPage(),
     );
   }

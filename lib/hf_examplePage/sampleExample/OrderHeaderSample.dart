0feebe7d64faa9fb9dfdc9b3af939ca6a1941f0c
Zhumz <zhumuzhi@icloud.com>
2020-09-25 11:24:58 +0800

    倒计时测试

--- lib/hf_examplePage/sampleExample/OrderHeaderSample.dart
+++ lib/hf_examplePage/sampleExample/OrderHeaderSample.dart
@@ -1,17 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/Support/utils/screen_utils.dart';
+import 'dart:math';
+import 'dart:async';
 
-class OrderHeaderPage extends StatelessWidget {
+class OrderHeaderPage extends StatefulWidget {
+  @override
+  _OrderHeaderPageState createState() => _OrderHeaderPageState();
+}
+
+class _OrderHeaderPageState extends State<OrderHeaderPage> {
+
+  String titleStr;
+  var baseCount;
+
+
   @override
   Widget build(BuildContext context) {
+
+    // String countStr = countMethod(1601200017000, 1600935348514);
+    // print('倒计时计算为：$countStr');
+
+    const period = const Duration(seconds: 1);
+
+    Timer.periodic(period, (timer) {
+
+      String timeStr = countStrMethod(baseCount);
+
+      //到时回调
+      // print('count：$baseCount');
+      // print('timeStr：$timeStr');
+
+      titleStr = timeStr;
+
+      setState(() {
+        countDownLabel(titleStr);
+      });
+
+      baseCount--;
+      if (baseCount <= 0) {
+        //取消定时器，避免无限回调
+        timer.cancel();
+        timer = null;
+        print('倒计时结束，计时器销毁');
+      }
+
+    });
+
     return Scaffold(
       appBar: AppBar(title: Text('订单详情')),
       body: ListView(children: [
         XFSOrderDetailsTitleItem(),
+        countDownLabel(titleStr)
       ]),
     );
+
   }
+
+
+  Widget countDownLabel (String showStr) {
+    return Container(
+      color: Colors.green,
+      height: 100,
+      child:Text('$showStr') ,
+    );
+  }
+
+
+  @override
+  void initState() {
+    // baseCount = (1601200017000 - 1600935348514) / 1000;
+    baseCount = (1601200059000 - 1601200017000) / 1000;
+    print('初始化倒计时赋值:$baseCount');
+    super.initState();
+  }
+
+  
+
+
 }
+
+
+countStrMethod (double secondtime){
+  double intDiff =  secondtime;
+  var day = 0, hour = 0, minute =0, second = 0;
+  String timeStr;
+  if (intDiff > 0){
+    day = (intDiff/ (60*60*24)).floor();
+    hour = ((intDiff / (60 * 60)) - (day * 24)).floor();
+    minute = ((intDiff / 60) - (day * 24 * 60) - (hour * 60)).floor();
+    second = ((intDiff) - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60)).floor();
+  }
+  if (hour <= 9) hour = 0 + hour;
+  if (minute <= 9) minute = 0 + minute;
+  if (second <= 9) second = 0 + second;
+  if (day > 0) {
+    if (day < 10) {
+      timeStr = '0' + '$day' + '天' +  '$hour' + '小时' + '$minute' + '分' + '$second' + '秒';
+    }
+  }else {
+    timeStr = '$hour' + '小时' + '$minute' + '分' + '$second' + '秒';
+  }
+  // intDiff--;
+  return timeStr;
+}
+
 
 /// ============================================ 标题Item ============================================
 class XFSOrderDetailsTitleItem extends StatelessWidget {
@@ -71,7 +163,9 @@ class XFSOrderDetailsTitleItem extends StatelessWidget {
       ]),
     );
   }
+
 }
+
 
 /// ============================================ 订单信息Item ============================================
 class XFSOrderDetailsOrderInfoItem extends StatelessWidget {
@@ -129,4 +223,5 @@ class XFSOrderDetailsOrderInfoItem extends StatelessWidget {
       ),
     );
   }
+
 }

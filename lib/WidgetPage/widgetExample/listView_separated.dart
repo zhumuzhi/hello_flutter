86c3adcae49c17f39ec033d7270002c8a205eb27
Zhumz <zhumuzhi@icloud.com>
2020-12-21 18:01:02 +0800

    1.ListView.Separated

--- lib/WidgetPage/widgetExample/listView_separated.dart
+++ lib/WidgetPage/widgetExample/listView_separated.dart
@@ -8,20 +8,26 @@ import 'package:flutter/material.dart';
 class ListViewSeparatedClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget divider1 = Divider(
+      color: Colors.blue,
+    );
+    Widget divider2 = Divider(color: Colors.green);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('ListViewSeparated'),
+        title: Text('ListView.Separated'),
       ),
-      body: Container(),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('$index'),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index){
+          return index%2==0 ? divider1 : divider2;
+        },
+        itemCount: 30,
+      ),
     );
   }
 }
-
- 
-
-
-
-
-
-
-      

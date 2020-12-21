8c8387a49b9fa618939a7bc3420c0f72d1e19bd8
Zhumz <zhumuzhi@icloud.com>
2020-12-21 09:17:30 +0800

    1.gridView

--- lib/WidgetPage/widgetExample/gridView_normal.dart
+++ lib/WidgetPage/widgetExample/gridView_normal.dart
@@ -13,20 +13,25 @@ class GridViewClass extends StatelessWidget {
         title: Text('GridView'),
       ),
       body: GridView(
-
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          childAspectRatio: 1.0, //宽高比为1:2
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 0,
+        ),
         children: [
-
+          Card(child: Icon(Icons.ac_unit)),
+          Card(child: Icon(Icons.airport_shuttle)),
+          Card(child: Icon(Icons.all_inclusive)),
+          Card(child: Icon(Icons.beach_access)),
+          Card(child: Icon(Icons.cake)),
+          Card(child: Icon(Icons.free_breakfast)),
+          Card(child: Icon(Icons.all_out)),
+          Card(child: Icon(Icons.battery_std_outlined)),
+          Card(child: Icon(Icons.beenhere_rounded)),
+          Card(child: Icon(Icons.collections_outlined)),
         ],
-
       ),
     );
   }
 }
-
-
-
-
-
-
-
-      
